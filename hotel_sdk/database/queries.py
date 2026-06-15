f0221882d50f440f618955e563837b194fe1c53c
G MAHESH <mahesh.dev07@kgrp.in>
2026-06-15 16:51:42 +0530

    Update SQL queries for hotel uploads

--- hotel_sdk/database/queries.py
+++ hotel_sdk/database/queries.py
@@ -3,11 +3,11 @@
 # """
 
 CHECK_DUPLICATE_FILE_HASH = """
-SELECT COUNT(*) as count FROM hotel_invoice WHERE file_hash = %(file_hash)s;
+SELECT COUNT(*) as count FROM hotel_uploads WHERE file_hash = %(file_hash)s;
 """
 
 INSERT_HOTEL_UPLOAD = """
-INSERT INTO hotel_invoice
+INSERT INTO hotel_uploads
 (id, file_url, source, source_id, client_name, file_hash, status, created_on, updated_on)
 VALUES (%(id)s, %(file_url)s, %(source)s, %(source_id)s, %(client_name)s,
         %(file_hash)s, %(status)s, %(created_on)s, %(updated_on)s)
